84ee055796effb9ee07c254dacd2f51f7c3bff29
Nathan Patrizi <nathan.patrizi@gmail.com>
2024-08-15 20:39:29 +0100

    Update for Python 3

--- photo2A4.py
+++ photo2A4.py
@@ -29,19 +29,19 @@ resultpadding = 10
 
 # Parse arguments
 def help():
-    print "USAGE:"
-    print sys.argv[0]+" [image1] [image2] [image3]"
-    print "Specify 1-3 image files to combine into an A4 image."
+    print("USAGE:")
+    print(sys.argv[0]+" [image1] [image2] [image3]")
+    print("Specify 1-3 image files to combine into an A4 image.")
     return
 
 if len(sys.argv) < 2:
     help()
-    quit()
+    sys.exit()
 
 if len(sys.argv) > 4:
-    print "ERROR: Please specify 1-3 image files."
+    print("ERROR: Please specify 1-3 image files.")
     help()
-    quit()
+    sys.exit()
 
 # Create blank result image
 result = Image.new('RGB', (resultsizex, resultsizey), 'white')
@@ -57,52 +57,56 @@ for image in sys.argv:
     try:
         img = Image.open(image)
     except:
-        print "ERROR: Cannot open image "+imageno
+        print("ERROR: Cannot open image "+imageno)
         traceback.print_exc()
-        quit()
+        sys.exit()
 
     # Get size
     width, height = img.size
-    print "IMAGE "+str(imageno)+" SIZE: "+str(width)+" x "+str(height)
+    print("IMAGE "+str(imageno)+" SIZE: "+str(width)+" x "+str(height))
 
     # Rotate if image is not portrait
     if width > height:
-        img = img.rotate(90)
+        print("ROTATING IMAGE")
+        img = img.rotate(90, expand=True)
         width, height = img.size
 
     # If image is larger than the target size, scale it down. Else, scale it up.
     if height > photoy:
-        print "SCALING DOWN"
+        print("SCALING DOWN")
         r = float(height)/float(photoy)
         r = float(width)/float(r)
-        img.thumbnail((int(r), photoy), Image.ANTIALIAS)
-        # Crop the image if we need to
-        width, height = img.size
-        if width > photox or height > photoy:
-            print "CROPPING IMAGE"
-            img = img.crop((((width - photox)/2), 0, ((width+photox)/2), photoy))
+        img.thumbnail((int(r), photoy), Image.LANCZOS)
     else:
         # If image is smaller, work out which side is closest to the target size and scale up until it hits the target
         photoxdiff = photox - width
         photoydiff = photoy - height
         if photoxdiff > photoydiff:
-            print "SCALING UP TO HEIGHT"
+            print("SCALING UP TO HEIGHT")
             r = float(photoy)/float(height)
             r = float(width)*float(r)
-            img = img.resize((int(r), photoy), Image.ANTIALIAS)
+            img = img.resize((int(r), photoy), Image.LANCZOS)
         else:
-            print "SCALING UP TO WIDTH"
+            print("SCALING UP TO WIDTH")
             r = float(photox)/float(width)
             r = float(height)*float(r)
-            img = img.resize((photox, int(r)), Image.ANTIALIAS)
+            img = img.resize((photox, int(r)), Image.LANCZOS)
 
     # Get new image size
     width, height = img.size
-    print "NEW IMAGE SIZE: "+str(width)+" x "+str(height)
+
+    # Crop the image if we need to
+    width, height = img.size
+    if width > photox or height > photoy:
+        print("CROPPING IMAGE")
+        img = img.crop((((width - photox)/2), 0, ((width+photox)/2), photoy))
+        width, height = img.size
+
+    print("NEW IMAGE SIZE: "+str(width)+" x "+str(height))
 
     # Rotate third image
     if imageno == 3:
-        img = img.rotate(90)
+        img = img.rotate(90, expand=True)
     else:
         bottomimagepos = photoy+(resultpadding*2)+toppadding+middlepadding
 
@@ -110,7 +114,7 @@ for image in sys.argv:
     if imageno == 1:
         result.paste(img, (resultpadding+leftpadding, resultpadding+toppadding))
     elif imageno == 2:
-        result.paste(img, ((resultsizex/2)+resultpadding, resultpadding+toppadding))
+        result.paste(img, (int((resultsizex/2)+resultpadding), resultpadding+toppadding))
     elif imageno == 3:
         result.paste(img, (resultpadding+leftpadding, bottomimagepos))
 
