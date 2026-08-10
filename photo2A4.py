bcbd943034292f3c0ccd7910924c11872b7dee43
Nathan Patrizi <nathan.patrizi@gmail.com>
2026-08-10 08:53:42 +0100

    Add option to set if images should be cropped

--- photo2A4.py
+++ photo2A4.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import getopt
 import traceback
 from PIL import Image
 
@@ -26,33 +27,46 @@ leftpadding = 50
 middlepadding = 50
 resultpadding = 10
 
+# Crop images to fit (fit to width)
+cropmode = False
 
 # Parse arguments
 def help():
     print("USAGE:")
-    print(sys.argv[0]+" [image1] [image2] [image3]")
+    print(sys.argv[0]+" [-c] [image1] [image2] [image3]")
     print("Specify 1-3 image files to combine into an A4 image.")
+    print("-c Enable cropping of images to fit (fit to width)")
     return
 
-if len(sys.argv) < 2:
+try:
+    optlist, args = getopt.getopt(sys.argv[1:], "c")
+except getopt.GetoptError as err:
+    sys.stderr.write(str(err)+"\n")
+    help()
+    sys.exit(1)
+
+for o, a in optlist:
+    if o == "-c":
+        cropmode = True
+    else:
+        sys.stderr.write("Unknown option: "+str(o)+"\n")
+        sys.exit(1)
+
+if len(args) < 1:
     help()
     sys.exit()
 
-if len(sys.argv) > 4:
-    print("ERROR: Please specify 1-3 image files.")
+if len(args) > 3:
+    sys.stderr.write("ERROR: Please specify 1-3 image files.")
     help()
-    sys.exit()
+    sys.exit(1)
 
 # Create blank result image
 result = Image.new('RGB', (resultsizex, resultsizey), 'white')
 
 # Main loop - open each image, resize, crop then paste onto result
 imageno = 0
-for image in sys.argv:
-    if imageno == 0:
-        imageno += 1
-        continue
-
+for image in args:
     # Try to open the input image
     try:
         img = Image.open(image)
@@ -71,25 +85,57 @@ for image in sys.argv:
         img = img.rotate(90, expand=True)
         width, height = img.size
 
-    # If image is larger than the target size, scale it down. Else, scale it up.
-    if width > photox:
-        print("SCALING DOWN")
-        r = float(width)/float(photox)
-        r = float(height)/float(r)
-        img.thumbnail((photox, int(r)), Image.LANCZOS)
+    if cropmode:
+        # If image is larger than the target size, scale it down. Else, scale it up.
+        if width > photox:
+            print("SCALING DOWN")
+            r = float(width)/float(photox)
+            r = float(height)/float(r)
+            img.thumbnail((photox, int(r)), Image.LANCZOS)
+        else:
+            # If image is smaller, scale it up until it fills the width
+            print("SCALING UP TO WIDTH")
+            r = float(photox)/float(width)
+            r = float(height)*float(r)
+            img = img.resize((photox, int(r)), Image.LANCZOS)
     else:
-        # If image is smaller, scale it up until it fills the width
-        print("SCALING UP TO WIDTH")
-        r = float(photox)/float(width)
-        r = float(height)*float(r)
-        img = img.resize((photox, int(r)), Image.LANCZOS)
+        if height > photoy:
+            print("SCALING DOWN")
+            r = float(height)/float(photoy)
+            r = float(width)/float(r)
+            img.thumbnail((int(r), photoy), Image.LANCZOS)
+        else:
+            # If image is smaller, work out which side is closest to the target size and scale up until it hits the target
+            photoxdiff = photox - width
+            photoydiff = photoy - height
+            if photoxdiff > photoydiff:
+                print("SCALING UP TO HEIGHT")
+                r = float(photoy)/float(height)
+                r = float(width)*float(r)
+                img = img.resize((int(r), photoy), Image.LANCZOS)
+            else:
+                print("SCALING UP TO WIDTH")
+                r = float(photox)/float(width)
+                r = float(height)*float(r)
+                img = img.resize((photox, int(r)), Image.LANCZOS)
+
+        width, height = img.size
+        if height > photoy:
+            print("SCALING DOWN TO HEIGHT")
+            r = float(height)/float(photoy)
+            r = float(width)/float(r)
+            img.thumbnail((int(r), photoy), Image.LANCZOS)
+        elif width > photox:
+            print("SCALING DOWN TO WIDTH")
+            r = float(width)/float(photox)
+            r = float(height)/float(r)
+            img.thumbnail((photox, int(r)), Image.LANCZOS)
 
     # Get new image size
     width, height = img.size
 
     # Crop the image if we need to
-    width, height = img.size
-    if width > photox or height > photoy:
+    if cropmode and (width > photox or height > photoy):
         print("CROPPING IMAGE")
         img = img.crop((((width - photox)/2), 0, ((width+photox)/2), photoy))
         width, height = img.size
@@ -97,17 +143,17 @@ for image in sys.argv:
     print("NEW IMAGE SIZE: "+str(width)+" x "+str(height))
 
     # Rotate third image
-    if imageno == 3:
+    if imageno == 2:
         img = img.rotate(90, expand=True)
     else:
         bottomimagepos = photoy+(resultpadding*2)+toppadding+middlepadding
 
     # Paste images onto the result
-    if imageno == 1:
+    if imageno == 0:
         result.paste(img, (resultpadding+leftpadding, resultpadding+toppadding))
-    elif imageno == 2:
+    elif imageno == 1:
         result.paste(img, (int((resultsizex/2)+resultpadding), resultpadding+toppadding))
-    elif imageno == 3:
+    elif imageno == 2:
         result.paste(img, (resultpadding+leftpadding, bottomimagepos))
 
     imageno += 1
@@ -119,3 +165,4 @@ if openresult:
     # Windows
     if os.name == "nt":
         os.system('rundll32 "C:\Program Files\Windows Photo Viewer\PhotoViewer.dll" ImageView_Fullscreen '+resultfile)
+
